cc31356738d42b8a494cd8494905d0aa72a2bac5
harishparuchuri <harishparuchuri007@gmail.com>
2020-07-05 11:16:59 +0530

    landing page done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/landing-page/header/header.component';
+import { CarouselComponent } from './components/landing-page/carousel/carousel.component';
+import { FooterComponent } from './components/landing-page/footer/footer.component';
+import { LandingPageComponent } from './components/landing-page/landing-page.component';
+import { RemainingComponent } from './components/landing-page/remaining/remaining.component';
+import { AboutComponent } from './components/landing-page/about/about.component';
+import { ChefComponent } from './components/landing-page/chef/chef.component';
+import { ContactComponent } from './components/landing-page/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    CarouselComponent, 
+    FooterComponent,
+    LandingPageComponent,
+    RemainingComponent,
+    AboutComponent,
+    ChefComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/landing-page/remaining/remaining.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-remaining',
+  templateUrl: './remaining.component.html',
+  styleUrls: ['./remaining.component.css']
+})
+export class RemainingComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
